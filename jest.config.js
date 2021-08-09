961081296625c07763afb71319205245461b98c2
Joaqim Planstedt <github@joaqim.xyz>
2021-08-09 12:34:59 +0200

    Remove tsconfig and jest alias paths (*: [src/*])

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,6 @@
-/* eslint-disable import/order */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("./tsconfig.json");
-const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
-  prefix: "<rootDir>/",
-});
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
@@ -17,6 +8,4 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
   },
-  moduleNameMapper,
-  modulePaths: ["<rootDir>/src"],
 };
